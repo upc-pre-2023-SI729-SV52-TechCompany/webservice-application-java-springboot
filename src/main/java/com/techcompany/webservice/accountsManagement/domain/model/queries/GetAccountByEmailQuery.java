3a0a2b6a778cfc9f7e18d4cb3d016d253b7eab1c
jackby03 <jackdelaguilavilla03@gmail.com>
2023-11-04 11:48:30 -0500

    feat: Delete Account

--- src/main/java/com/techcompany/webservice/accountsManagement/domain/model/queries/GetAccountByEmailQuery.java
+++
@@ -1,6 +0,0 @@
-package com.techcompany.webservice.accountsManagement.domain.model.queries;
-
-import com.techcompany.webservice.accountsManagement.domain.model.valueobjects.EmailAddress;
-
-public record GetAccountByEmailQuery(EmailAddress email) {
-}
