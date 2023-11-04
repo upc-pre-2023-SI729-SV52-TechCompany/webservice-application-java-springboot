8c07b6ae58e13e4779837dba767dcd7a5c72c665
jackby03 <jackdelaguilavilla03@gmail.com>
2023-11-04 11:42:24 -0500

    feat: Implement entities from AccountManagement

---
+++ src/main/java/com/techcompany/webservice/accountsManagement/domain/model/entities/Client.java
@@ -0,0 +1,39 @@
+package com.techcompany.webservice.accountsManagement.domain.model.entities;
+
+import com.techcompany.webservice.accountsManagement.domain.model.valueobjects.*;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Embedded
+    private EmailAddress email;
+    @Embedded
+    private Password password;
+    @Embedded
+    private PersonName fullName;
+    @Embedded
+    private PhoneNumber phone;
+    @Embedded
+    private Birthdate birthdate;
+    @Embedded
+    private IdNumber idNumber;
+    @Embedded
+    private Age age;
+    @Embedded
+    private Country country;
+    @Embedded
+    private Photo photo;
+    @Embedded
+    private Username username;
+    @Embedded
+    private UserDescription userDescription;
+}
